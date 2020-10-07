a4e591f5975159e3b6b877f0c9ae857655c7c52d
Abdullah Al-Ashi <abdullah.alashi@gmail.com>
2020-10-07 21:22:42 +1100

    -  create new empty project

---
+++ XplorRobot/XplorRobot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XplorRobot
+//
+//  Created by Abdullah Al-Ashi on 7/Oct/20.
+//  Copyright © 2020 Abdullah Al-Ashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
